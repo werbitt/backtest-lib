8ac9a58a0ae991670188a0dcc001f4a36b163d13
Micah Werbitt <micah@werbitt.net>
2016-02-14 16:18:32 -0500

    run function to evaluate the application monad

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    (
+      run
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Control.Monad.Reader (runReaderT)
+import           Rd.Query             (connection, lastHistoryVersion)
+import           Rd.Types             (Backtest, Env (..))
+
+run :: Backtest a -> IO a
+run m = do
+  conn <- connection
+  version <- lastHistoryVersion conn
+  runReaderT m $ Env conn version
