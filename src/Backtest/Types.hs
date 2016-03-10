d3c0209024f7104d2940917ceb460f6e8a6b3c80
Micah Werbitt <micah@werbitt.net>
2016-03-10 07:43:17 -0500

    Use classy monad transformer stack

--- src/Backtest/Types.hs
+++ src/Backtest/Types.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DeriveFoldable             #-}
 {-# LANGUAGE DeriveFunctor              #-}
 {-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE FunctionalDependencies     #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE RankNTypes                 #-}
@@ -10,15 +12,21 @@
 module Backtest.Types
        ( Backtest
        , unBacktest
+       , AppConfig (..)
+       , BacktestConfig (..)
+       , backtestConfig
+       , HasBacktestConfig
+       , dbConfig
+       , HasDbConfig
          -- * Reader Environment
-       , Env
-       , mkEnv
+--       , Env
+--       , mkEnv
        , connection
        , historyVersion
-       , params
+--       , params
          -- * Optimization Parameters
-       , Params
-       , mkParams
+--       , Params
+--       , mkParams
        , startDate
        , startValue
        , frequency
@@ -45,7 +53,6 @@ module Backtest.Types
        , Weight
          -- * Strategy
        , Strategy
-       , mkStrategy
        , getData
        , rank
          -- * Constraints
@@ -78,113 +85,10 @@ import           Database.PostgreSQL.Simple (Connection)
 import           Opaleye.PGTypes            (PGText)
 import           Opaleye.RunQuery           (QueryRunnerColumnDefault)
 
-newtype Backtest a = Backtest { unBacktest :: ReaderT Env IO a } deriving
-                     (
-                       Functor
-                     , Applicative
-                     , Monad
-                     , MonadReader Env
-                     , MonadIO
-                     )
 
-data Env = Env { _connection     :: Connection
-               , _historyVersion :: Int
-               , _params         :: Params }
-
-
-mkEnv :: Connection -> Int -> Params ->  Env
-mkEnv = Env
-
-
-------------------------------------------------------------------------
--- | Optimization Parameters
-------------------------------------------------------------------------
-
-type Cutoff = Double
-type Buffer = Double
-
-data Params = Params { _startDate  :: Day
-                     , _startValue :: Value
-                     , _frequency  :: Frequency
-                     , _cutoff     :: Cutoff
-                     , _buffer     :: Buffer }
-
-mkParams :: Day -> Value -> Frequency -> Cutoff -> Buffer -> Params
-mkParams = Params
-
-------------------------------------------------------------------------
--- | Assets
-------------------------------------------------------------------------
-newtype Ticker = Ticker String deriving (Show, Eq, Ord)
-deriving instance QueryRunnerColumnDefault PGText Ticker
-
-mkTicker :: String -> Ticker
-mkTicker = Ticker
-
-unTicker :: Ticker -> String
-unTicker (Ticker s) = s
-
-data Asset = Cash | Equity Ticker deriving (Show, Eq, Ord)
-
-mkCash :: Asset
-mkCash = Cash
-
-mkEquity :: Ticker -> Asset
-mkEquity = Equity
-
-getTicker :: Asset -> Maybe Ticker
-getTicker (Equity t) = Just t
-getTicker _          = Nothing
-
-
-type Price = Double
-type Return = Double
-
-class HasAsset a where
-  getAsset :: a -> Asset
-------------------------------------------------------------------------
--- | Portfolios
-------------------------------------------------------------------------
-type Value = Double
-type Weight = Double
-
-data PortfolioF a = PortfolioF (Map Asset a) deriving (Functor, Foldable, Show)
-type Portfolio = PortfolioF Value
-type PortfolioW = PortfolioF Weight
-
-mkPortfolio :: (Num a, Eq a) => [(Asset, a)] -> PortfolioF a
-mkPortfolio = PortfolioF . M.fromList
-
-
-------------------------------------------------------------------------
--- | Stragegies
-------------------------------------------------------------------------
-data Strategy m a = Strategy
-                  { _getData :: Day -> m [a]
-                  , _rank    :: [a] -> [a]  }
-
-mkStrategy ::
-  (MonadReader Env m, MonadIO m) =>
-  (Day -> m [a]) -> ([a] -> [a]) -> Strategy m a
-mkStrategy = Strategy
-
-------------------------------------------------------------------------
--- | Constraints
-------------------------------------------------------------------------
-type Constraint a = a -> Constrain
-
-data Constraints a = Constraints { _global :: [Constraint a]
-                                 , _short  :: [Constraint a]
-                                 , _long   :: [Constraint a] }
-
-mkConstraints :: [Constraint a] -> [Constraint a] -> [Constraint a] -> Constraints a
-mkConstraints = Constraints
-
-data Constrain = Include | Exclude deriving (Eq)
 ------------------------------------------------------------------------
 -- | Dates
 ------------------------------------------------------------------------
-
 data Ordinal = First
              | Second
              | Third
@@ -219,23 +123,124 @@ weekdayToInt Sunday    = 7
 data Frequency = Monthly { _ordinal :: Ordinal
                          , _weekday :: Weekday
                          , _wait    :: Wait } deriving (Show)
+makeLenses ''Frequency
 
 mkFrequency :: Ordinal -> Weekday -> Wait -> Frequency
 mkFrequency = Monthly
 
 
+------------------------------------------------------------------------
+-- | Assets
+------------------------------------------------------------------------
+newtype Ticker = Ticker String deriving (Show, Eq, Ord)
+deriving instance QueryRunnerColumnDefault PGText Ticker
+
+mkTicker :: String -> Ticker
+mkTicker = Ticker
+
+unTicker :: Ticker -> String
+unTicker (Ticker s) = s
+
+data Asset = Cash | Equity Ticker deriving (Show, Eq, Ord)
+
+mkCash :: Asset
+mkCash = Cash
+
+mkEquity :: Ticker -> Asset
+mkEquity = Equity
+
+getTicker :: Asset -> Maybe Ticker
+getTicker (Equity t) = Just t
+getTicker _          = Nothing
+
+
+type Price = Double
+type Return = Double
+
+class HasAsset a where
+  getAsset :: a -> Asset
+
 instance HasAsset Asset where
   getAsset = id
 
 
+------------------------------------------------------------------------
+-- | Portfolios
+------------------------------------------------------------------------
+type Value = Double
+type Weight = Double
+
+data PortfolioF a = PortfolioF (Map Asset a) deriving (Functor, Foldable, Show)
+type Portfolio = PortfolioF Value
+type PortfolioW = PortfolioF Weight
+
+mkPortfolio :: (Num a, Eq a) => [(Asset, a)] -> PortfolioF a
+mkPortfolio = PortfolioF . M.fromList
+
 
 ------------------------------------------------------------------------
--- | Lenses
+-- | Stragegies
 ------------------------------------------------------------------------
+data Strategy m a = Strategy
+                  { _getData :: Day -> m [a]
+                  , _rank    :: [a] -> [a]  }
 
-makeLenses ''Params
-
-makeLenses ''Frequency
-makeLenses ''Constraints
 makeLenses ''Strategy
-makeClassy ''Env
+
+
+------------------------------------------------------------------------
+-- | Constraints
+------------------------------------------------------------------------
+data Constrain = Include | Exclude deriving (Eq)
+type Constraint a = a -> Constrain
+
+data Constraints a = Constraints { _global :: [Constraint a]
+                                 , _short  :: [Constraint a]
+                                 , _long   :: [Constraint a] }
+
+makeLenses ''Constraints
+
+mkConstraints :: [Constraint a] -> [Constraint a] -> [Constraint a] -> Constraints a
+mkConstraints = Constraints
+
+
+------------------------------------------------------------------------
+-- | Config
+------------------------------------------------------------------------
+data DbConfig = DbConfig { _connection     :: Connection
+                         , _historyVersion :: Int }
+makeClassy ''DbConfig
+
+
+type Cutoff = Double
+type Buffer = Double
+data BacktestConfig = BacktestConfig { _startDate  :: Day
+                                     , _startValue :: Value
+                                     , _frequency  :: Frequency
+                                     , _cutoff     :: Cutoff
+                                     , _buffer     :: Buffer }
+
+makeClassy ''BacktestConfig
+
+data StrategyConfig m a = StrategyConfig { _strategy    :: Strategy m a
+                                         , _constraints :: Constraints a }
+
+
+data AppConfig  = AppConfig { appDbConfig       :: DbConfig
+                            , appBacktestConfig :: BacktestConfig }
+makeClassy ''AppConfig
+
+
+
+
+------------------------------------------------------------------------
+-- | Application Monad Stack
+------------------------------------------------------------------------
+newtype Backtest a = Backtest { unBacktest :: ReaderT AppConfig IO a } deriving
+                     (
+                       Functor
+                     , Applicative
+                     , Monad
+                     , MonadReader AppConfig
+                     , MonadIO
+                     )
--- src/Lib.hs
+++ src/Lib.hs
@@ -5,12 +5,16 @@ module Lib
       run
     ) where
 
+import qualified Backtest.Query       as Q
+import           Backtest.Types       (AppConfig, Backtest, unBacktest)
 import           Control.Monad.Reader (runReaderT)
-import qualified Rd.Query             as Q
-import           Rd.Types             (Backtest, Params, mkEnv, unBacktest)
 
-run :: Params -> Backtest a -> IO a
-run params m = do
+
+
+
+run :: AppConfig -> Backtest a -> IO a
+run config m = do
   conn <- Q.connection
   version <- Q.lastHistoryVersion conn
-  runReaderT (unBacktest m) (mkEnv conn version params)
+  -- update conn and version in config
+  runReaderT (unBacktest m) config
