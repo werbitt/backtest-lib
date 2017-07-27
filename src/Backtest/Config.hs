8ee0b4e200cebcfd683ee07299f52ee9656ff76e
Micah Werbitt <micah@werbitt.net>
2017-07-27 07:46:23 -0400

    Update license and exposed modules

--- src/Backtest/Config.hs
+++ src/Backtest/Config.hs
@@ -1,19 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+{-}
+Loading configuration for a backtest. Configuration includes database connection
+info and backtest configuration. Configuation may have a default, then they will
+be loaded from the app.cfg file, then they can be overwritten by commnand line
+args.
+-}
+
 module Backtest.Config
-       (
+       ( DbConfigSource (..)
+       , BacktestConfigSource(..)
        ) where
 
-import           Backtest.Types          (Buffer, Cutoff, Frequency,
+import           Backtest.Types          (Buffer, Cutoff, DbConfig, Frequency,
                                           Ordinal (..), Value, Weekday (..),
                                           mkFrequency)
 
 import           Data.Configurator       as C
-import           Data.Configurator.Types (Config, Worth (..))
+import           Data.Configurator.Types (Config)
 import           Data.Function           (on)
 import           Data.List               (foldl1')
 import           Data.Maybe              (catMaybes, listToMaybe)
-import           Data.Monoid             (Last (..), getLast, mappend, (<>))
+import           Data.Semigroup          (Last (..), Semigroup, getLast, (<>))
 import           Data.String             (fromString)
 import           Data.Text               (Text)
 import           Data.Time               (Day, defaultTimeLocale, fromGregorian,
@@ -29,111 +37,110 @@ loadConfig :: String
            -> FilePath
            -- ^ The directory to look in
            -> IO Config
-loadConfig file dir =  C.load $ [Required (dir </> file)]
+loadConfig file dir =  C.load [Required (dir </> file)]
+
+
+class Semigroup a => ConfigSource a where
+  def :: a
+  extract :: Config -> IO a
+
+  -- | Start with default config, then update with any config in the first
+  -- parameter, this is from the config file for instance, then update from
+  -- anything from the second parameter, these are command line options for
+  -- instance
+  init :: Maybe a -> Maybe a -> a
+  init c c' = foldl1' (<>) $
+    def : catMaybes [c, c']
 
 
 --------------------------------------------------------------------------------
--- | Data structure for holdings configuration for connecting to
+-- | Data structure for holdings possible configuration for connecting to
 -- Postgres database
-data DbConfig = DbConfig { _dbConfigHost     :: Maybe String
-                         , _dbConfigPort     :: Maybe Int
-                         , _dbConfigUser     :: Maybe String
-                         , _dbConfigPassword :: Maybe String
-                         , _dbConfigDatabase :: Maybe String }
+data DbConfigSource
+  = DbConfigSource { _dbConfigHost     :: Maybe String
+                   , _dbConfigPort     :: Maybe Int
+                   , _dbConfigUser     :: Maybe String
+                   , _dbConfigPassword :: Maybe String
+                   , _dbConfigDatabase :: Maybe String }
 
---------------------------------------------------------------------------------
+
 -- | Monoid instance for DbConfig. If there is a value on the RHS it will
 -- overwrite.
-instance Monoid DbConfig where
-  mempty = DbConfig { _dbConfigHost     = Nothing
-                    , _dbConfigPort     = Nothing
-                    , _dbConfigUser     = Nothing
-                    , _dbConfigPassword = Nothing
-                    , _dbConfigDatabase = Nothing }
-  a `mappend` b = DbConfig
-      { _dbConfigHost     = ov _dbConfigHost
-      , _dbConfigPort     = ov _dbConfigPort
-      , _dbConfigUser     = ov _dbConfigUser
-      , _dbConfigPassword = ov _dbConfigPassword
-      , _dbConfigDatabase = ov _dbConfigDatabase
-      }
+instance Semigroup DbConfigSource where
+  a <> b = DbConfigSource { _dbConfigHost     = ov _dbConfigHost
+                          , _dbConfigPort     = ov _dbConfigPort
+                          , _dbConfigUser     = ov _dbConfigUser
+                          , _dbConfigPassword = ov _dbConfigPassword
+                          , _dbConfigDatabase = ov _dbConfigDatabase }
     where
-      ov f = getLast $! (mappend `on` (Last . f)) a b
+      ov f = getLast $ ((<>) `on` (Last . f)) a b
 
---------------------------------------------------------------------------------
--- | Default settings for Postgres connection
-defaultDbConfig :: DbConfig
-defaultDbConfig = DbConfig { _dbConfigHost     = Just "localhost"
-                           , _dbConfigPort     = Just 5432
-                           , _dbConfigUser     = Just "backtest"
-                           , _dbConfigPassword = Just ""
-                           , _dbConfigDatabase = Just "backtest" }
+instance ConfigSource DbConfigSource where
+  def = DbConfigSource { _dbConfigHost     = Just "localhost"
+                       , _dbConfigPort     = Just 5432
+                       , _dbConfigUser     = Just "backtest"
+                       , _dbConfigPassword = Just ""
+                       , _dbConfigDatabase = Just "backtest" }
 
---------------------------------------------------------------------------------
--- | Transform Configurator db.connection group into DbConfig
-getDbConfig :: Config -> IO DbConfig
-getDbConfig cfg = do
-  host <- C.lookup cfg "db.connectInfo.host"
-  port <- C.lookup cfg "db.connectInfo.port"
-  user <- C.lookup cfg "db.connectInfo.user"
-  pass <- C.lookup cfg "db.connectInfo.password"
-  db   <- C.lookup cfg "db.connectInfo.database"
-  return $ DbConfig host port user pass db
+  extract c = do
+    host <- C.lookup c "db.connectInfo.host"
+    port <- C.lookup c "db.connectInfo.port"
+    user <- C.lookup c "db.connectInfo.user"
+    pass <- C.lookup c "db.connectInfo.password"
+    db   <- C.lookup c "db.connectInfo.database"
+    return $ DbConfigSource host port user pass db
 
---------------------------------------------------------------------------------
--- | Combine default, file, and source configuration
-initDbConfig :: Maybe DbConfig
-             -- ^ Database config from file
-             -> Maybe DbConfig
-             -- ^ Database config in source
-             -> DbConfig
-initDbConfig cfg cfg' =  foldl1' mappend $
-  defaultDbConfig : catMaybes [cfg, cfg']
 
 --------------------------------------------------------------------------------
 -- | Data structure for common backtest configuration
-data BacktestConfig
-  = BacktestConfig { _bcDescription :: Maybe Text
-                   , _bcStartDate   :: Maybe Day
-                   , _bcStartValue  :: Maybe Value
-                   , _bcFrequency   :: Maybe Frequency
-                   , _bcCutoff      :: Maybe Cutoff
-                   , _bcBuffer      :: Maybe Buffer
-                   }
+data BacktestConfigSource
+  = BacktestConfigSource { _bcDescription :: Maybe Text
+                         , _bcStartDate   :: Maybe Day
+                         , _bcStartValue  :: Maybe Value
+                         , _bcFrequency   :: Maybe Frequency
+                         , _bcCutoff      :: Maybe Cutoff
+                         , _bcBuffer      :: Maybe Buffer
+                         }
 
---------------------------------------------------------------------------------
--- | Default backtest configuration
-defaultBacktestConfig :: BacktestConfig
-defaultBacktestConfig
-  = BacktestConfig { _bcDescription = Just "Backtest"
-                   , _bcStartDate   = Just $ fromGregorian 2016 1 1
-                   , _bcStartValue  = Just 1000000
-                   , _bcFrequency   = Just $ mkFrequency Third Friday 2
-                   , _bcCutoff      = Just 0.5
-                   , _bcBuffer      = Just 0
-                   }
-
---------------------------------------------------------------------------------
--- | Tranform configurator backtest group into Backtest config
-getBacktestConfig :: Config -> IO BacktestConfig
-getBacktestConfig cfg = do
-  desc <- C.lookup cfg "backtest.description"
-  sd <- do
-    sd' <- C.lookup cfg "backtest.start-date"
-    return $ sd' >>= parseDay
-  sv <- C.lookup cfg "backtest.start-value"
-
-  o <- C.lookup cfg "backtest.frequency.ordinal"
-  wd <- C.lookup cfg "backtest.frequency.weekday"
-  wt <- C.lookup cfg "backtest.frequency.wait"
-  let freq = case (o, wd, wt) of
-        (Just o', Just wd', Just wt') -> Just $ parseFrequency o' wd' wt'
-        _ -> Nothing
+instance Semigroup BacktestConfigSource where
+  a <> b = BacktestConfigSource { _bcDescription = ov _bcDescription
+                                , _bcStartDate = ov _bcStartDate
+                                , _bcStartValue = ov _bcStartValue
+                                , _bcFrequency = ov _bcFrequency
+                                , _bcCutoff = ov _bcCutoff
+                                , _bcBuffer = ov _bcBuffer
+                                }
+    where
+      ov f = getLast $ ((<>) `on` (Last . f)) a b
 
 
-  cut <- C.lookup cfg "backtest.cutoff"
-  buf <- C.lookup cfg "backtest.buffer"
-  return $ BacktestConfig desc sd sv freq cut buf
+instance ConfigSource BacktestConfigSource where
+  def = BacktestConfigSource { _bcDescription = Just "Backtest"
+                             , _bcStartDate   = Just $ fromGregorian 2016 1 1
+                             , _bcStartValue  = Just 1000000
+                             , _bcFrequency   = Just $ mkFrequency Third Friday 2
+                             , _bcCutoff      = Just 0.5
+                             , _bcBuffer      = Just 0
+                             }
+
+  extract c = do
+    desc <- C.lookup c "backtest.description"
+    sd <- do
+      sd' <- C.lookup c "backtest.start-date"
+      return $ sd' >>= parseDay
+
+    sv <- C.lookup c "backtest.start-value"
+    o  <- C.lookup c "backtest.frequency.ordinal"
+    wd <- C.lookup c "backtest.frequency.weekday"
+    wt <- C.lookup c "backtest.frequency.wait"
+    let freq = case (o, wd, wt) of
+          (Just o', Just wd', Just wt') -> Just $ parseFrequency o' wd' wt'
+          _ -> Nothing
+
+    cut <- C.lookup c "backtest.cutoff"
+    buf <- C.lookup c "backtest.buffer"
+    return $ BacktestConfigSource desc sd sv freq cut buf
+
 
 parseDay :: String -> Maybe Day
 parseDay s = firstJust tryParsers
@@ -153,4 +160,5 @@ parseFrequency :: String
                -> Int
                -- ^ Wait
                -> Frequency
-parseFrequency o wd wt = mkFrequency (fromString o) (fromString wd) wt
+parseFrequency ordinal weekday
+  = mkFrequency (fromString ordinal) (fromString weekday)
